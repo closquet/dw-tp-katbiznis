b9770506072154a0bb6674320a80d272eba11a5d
Eric Closquet <closquet.eric@live.be>
2017-02-09 11:42:55 +0100

    add assets

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,17 @@
 */
 
 var gulp = require("gulp");
+var image = require("gulp-image");
 
 // --- Task for images
 
-// --- Task for images
+gulp.task( "images", function () {
+    gulp.src( 'src/images/**' )
+    .pipe( image() )
+    .pipe( gulp.dest("assets/images") )
+} );
+
+// --- Task for styles
 
 // --- Task for images
 
