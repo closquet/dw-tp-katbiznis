186b514f50e95b7a7d303fc2c86bef87b5f8ed61
Eric Closquet <closquet.eric@live.be>
2017-02-09 11:17:40 +0100

    add assets

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/* eric/katbiznis
+*
+* /gulpfile.js - gulp tasks
+*
+* code by email...
+* started at 09/02/2017
+*
+*/
+
+var gulp = require("gulp");
+
+// --- Task for images
+
+// --- Task for images
+
+// --- Task for images
+
+// --- Task for images
+
+// --- Task for images
+
+// --- Task for images
